dac1de766f649eae7cf2ccd8b7df5da17d8fc197
Vyankatesh Rohokale <piyushrohokale2525@gmail.com>
2025-08-11 03:03:57 +0530

    Add environment configuration for production and development

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  BACKEND_URL: process.env['BACKEND_URL'] || 'https://your-backend-url.com'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = 
+{
+  production: false,
+  BACKEND_URL: process.env['BACKEND_URL']|| 'http://localhost:8000'
+};
+
